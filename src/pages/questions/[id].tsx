2b02333a37e4b4f787a3ec57db6209ad8ff80dbf
igor0400 <ulanov.igor0400@gmail.com>
2023-03-27 01:18:21 +0300

    commit

--- src/pages/questions/[id].tsx
+++ src/pages/questions/[id].tsx
@@ -2,12 +2,13 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { PageWrapper } from '../../shared';
 
-
 const QuestionPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <PageWrapper title="Prepise » Профиль">Вопрос {id}</PageWrapper>;
+  return (
+    <PageWrapper title={`Prepise » Вопрос ${id}`}>Вопрос {id}</PageWrapper>
+  );
 };
 
 export default QuestionPage;
