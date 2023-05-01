79c231d417334746a15aa10601c84a0ad5914954
igor0400 <ulanov.igor0400@gmail.com>
2023-05-01 23:22:09 +0300

    change next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,19 @@ module.exports = {
   reactStrictMode: false,
   images: {
     domains: ['api.prepise.com', 'prepise.com', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.prepise.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
